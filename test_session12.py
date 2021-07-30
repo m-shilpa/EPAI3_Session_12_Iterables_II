43684152a39896a85b84962f445422a00621f0a7
Shilpa M <36926868+m-shilpa@users.noreply.github.com>
2021-07-31 03:24:25 +0530

    Update test_session12.py

--- test_session12.py
+++ test_session12.py
@@ -25,6 +25,8 @@ def test_readme_contents():
     readme = open(README_PATH, "r")
     readme_words = readme.read().split()
     readme.close()
+    print(readme_words)
+    print(len(readme_words))
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 def test_readme_proper_description():
